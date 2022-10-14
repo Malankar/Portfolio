b16e78595895cb8eb65624f407aea327ace04f78
Avdhut Malankar <avdhut.satish@gmail.com>
2022-10-14 22:35:20 +0530

    changed bio

--- components/Hero.js
+++ components/Hero.js
@@ -7,13 +7,17 @@ const Hero = () => {
         <div className="max-w-sm m-auto">
           <div className="relative w-[375px] h-[375px] md:w-[500px]  md:h-[500px]">
             <div className="absolute ">
-              <img src="/blob.svg" alt="" className="w-[450px] h-[450px]" />
+              <img
+                src="/blob.svg"
+                alt=""
+                className="w-[450px] h-[450px] select-none"
+              />
             </div>
             <div className="absolute z-5 md:top-28 md:left-28 top-16 left-14">
               <img
                 src="/coder.svg"
                 alt=""
-                className="w-[250px] mt-5 md:mt-0 h-[250px] md:w-[275px] md:h-[275px] "
+                className="w-[250px] mt-5 md:mt-0 h-[250px] md:w-[275px] md:h-[275px] select-none"
               />
             </div>
           </div>
@@ -23,8 +27,9 @@ const Hero = () => {
             Hello, I&apos;m Avdhut Malankar
           </h1>
           <p className="text-lg md:text-xl pt-2 dark:text-gray-400">
-            I am a full stack developer, programmer who loves designing and
-            creating websites.
+            {/* I am a full stack developer, programmer who loves designing and creating websites. */}
+            I am a versatile individual and enthusiastic learner who enjoys
+            developing and designing stuff.
           </p>
           <a href="/Avdhut Malankar Resume.pdf" download>
             <div className="flex space-x-3  rounded-lg mt-5 w-fit bg-black text-white dark:bg-blue-600 cursor-pointer">
